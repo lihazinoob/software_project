f58222723056984acabd37d10cc9a73e690e948a
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2024-03-30 17:09:46 +0600

    User Model class is created to handle the user information during registration time.

---
+++ app/src/main/java/com/example/software_project/Models/UserModel.java
@@ -0,0 +1,26 @@
+package com.example.software_project.Models;
+
+public class UserModel {
+    private String image = null;
+    private String name = null;
+    private String email = null;
+    private String password = null;
+
+    public UserModel()
+    {
+        //this is the default constructor
+    }
+
+    public UserModel(String image, String name, String email, String password) {
+        this.image = image;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public UserModel(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
