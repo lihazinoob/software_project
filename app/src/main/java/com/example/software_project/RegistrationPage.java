7230074726dbed745b63eaab05a256e3970ec6f8
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2024-03-31 17:58:02 +0600

    Registration Logic is being implemented

--- app/src/main/java/com/example/software_project/RegistrationPage.java
+++ app/src/main/java/com/example/software_project/RegistrationPage.java
@@ -1,7 +1,10 @@
 package com.example.software_project;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+import android.window.SplashScreen;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,28 @@ public class RegistrationPage extends AppCompatActivity {
         EdgeToEdge.enable(this);
         View root = binding.getRoot();
         setContentView(root);
+
+        binding.registrationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Code to register user
+
+                if(binding.usernameEditText.getText().toString().isEmpty() || binding.passwordEditText.getText().toString().isEmpty() || binding.emailEditText.getText().toString().isEmpty())
+                {
+                    // Show error message
+                    Toast.makeText(RegistrationPage.this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    // Register user
+                    // Code to register user
+                    // Redirect to login page
+                    Toast.makeText(RegistrationPage.this, "User registered successfully", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+
     }
 }
