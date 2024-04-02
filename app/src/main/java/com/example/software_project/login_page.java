6c414d651c60f2ecc58c78121a6755c0a99917f8
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2024-04-02 16:51:17 +0600

    Login Page to registration page intent is done.

--- app/src/main/java/com/example/software_project/login_page.java
+++ app/src/main/java/com/example/software_project/login_page.java
@@ -1,6 +1,8 @@
 package com.example.software_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +10,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class login_page extends AppCompatActivity {
+import com.example.software_project.databinding.ActivityLoginPageBinding;
 
+public class login_page extends AppCompatActivity {
+    private ActivityLoginPageBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login_page);
+        binding = ActivityLoginPageBinding.inflate(getLayoutInflater());
+        View root = binding.getRoot();
+        setContentView(root);
+
+        binding.registrationPageLinkFromLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(login_page.this, RegistrationPage.class));
+            }
+        });
 
     }
 }
